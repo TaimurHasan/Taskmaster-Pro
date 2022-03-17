df57b759237b1a0482e756da804ea5f5296aeb43
TaimurHasan <taimurhasan11@gmail.com>
2022-03-16 20:38:05 -0400

    added jquery ui to make lists sortable and make droppable trash area function

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,6 +122,59 @@ $(".list-group").on("blur", "input[type='text']", function() {
     $(this).replaceWith(taskSpan);
 })
 
+
+// make list items sortable
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
+    console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store task data in
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim()
+
+      // add data to temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      })
+    });
+
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    tasks[arrName] = tempArr;
+    saveTasks();
+    
+  }
+});
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -165,6 +218,21 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
+// remove task dragged to drop zone
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+})
 // load tasks for the first time
 loadTasks();
 
